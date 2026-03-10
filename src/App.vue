56b19e5144be247ab8eeb796f066a224da9694b6
simonity <simonity3000@gmail.com>
2026-03-11 01:30:08 +0200

    Add App.vue with page layout and sections

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppHeader from "./components/AppHeader.vue";
+import HeroSection from "./components/HeroSection.vue";
+import ServicesSection from "./components/ServicesSection.vue";
+import AppFooter from "./components/AppFooter.vue";
+</script>
+
+<template>
+  <AppHeader />
+
+  <main>
+    <HeroSection />
+    <ServicesSection />
+  </main>
+
+  <AppFooter />
+</template>
