07ac0704ee9a5887f7d13a07821857f807a56781
simonity <simonity3000@gmail.com>
2026-03-12 23:49:49 +0200

    Add TestimonialsSection

---
+++ src/components/TestimonialsSection.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppSection from "./AppSection.vue";
+</script>
+
+<template>
+  <AppSection
+    title="Testimonials"
+    description="Hear from Our Satisfied Clients: Read Our Testimonials to Learn More about Our Digital Marketing Services"
+    descriptionWidth="465px"
+  >
+  </AppSection>
+</template>
+
+<style scoped>
+</style>
