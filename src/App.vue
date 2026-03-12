be3d90c70e4948a6b3ffe3f84916124f8dfffa1e
simonity <simonity3000@gmail.com>
2026-03-12 23:45:05 +0200

    Update App layout

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 import AppHeader from "./components/AppHeader.vue";
 import HeroSection from "./components/HeroSection.vue";
 import ServicesSection from "./components/ServicesSection.vue";
+import CaseSection from "./components/CaseStudiesSection.vue";
+import ProcessSection from "./components/ProcessSection.vue";
+import TeamSection from "./components/TeamSection.vue";
+import TestimonialsSection from "./components/TestimonialsSection.vue";
+import ContactSection from "./components/ContactSection.vue";
 import AppFooter from "./components/AppFooter.vue";
 </script>
 
@@ -11,6 +16,11 @@ import AppFooter from "./components/AppFooter.vue";
   <main>
     <HeroSection />
     <ServicesSection />
+    <CaseSection />
+    <ProcessSection />
+    <TeamSection />
+    <TestimonialsSection />
+    <ContactSection />
   </main>
 
   <AppFooter />
